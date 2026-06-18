81dc8ac19d03a7b54fb771bb0370df8221f24889
piyush singh <25116cn393@glbitm.ac.in>
2026-06-18 13:47:26 +0530

    Add files via upload

---
+++ day-18/q71_binary_search.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int target;
+    cin >> target;
+
+    int left = 0, right = n - 1;
+
+    while(left <= right) {
+        int mid = (left + right) / 2;
+
+        if(arr[mid] == target) {
+            cout << "Found at index " << mid;
+            return 0;
+        }
+        else if(arr[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+
+    cout << "Not Found";
+
+    return 0;
+}
